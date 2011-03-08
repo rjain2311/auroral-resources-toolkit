617e6c8fba1b931c93d143d87a4d740a6a2c6173
peterelespuru <peterelespuru@4889a53b-78bf-f7fc-ca64-26766ea9d849>
2011-03-08 17:07:40 +0000

    added thumbnail creation to the python code, updated accordingly

--- trunk/source/resource/auroral_resources/chapman2011/img.py
+++ trunk/source/resource/auroral_resources/chapman2011/img.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-import glob, csv, re
+import glob, csv, re, os, Image
 
 images = []
 thumbs = []
 metaTitle = {}
 metaDescr = {}
+initialImage = "20110301_010133-1000_JFS.jpg"
+iInitial = 0
 
 # grab them all
 raw_images = glob.glob('*.jpg')
@@ -23,27 +25,39 @@ images = [img for img in raw_images if img not in thumbs]
 # grab the metadata for later reference
 metadata = csv.reader(open("img.csv", "rb"))
 for row in metadata:
-  metaTitle[row[0]] = row[1]
-  metaDescr[row[0]] = row[2]
+  # the csv sometimes includes './' in the file name column
+  key = row[0].replace('./', '')
+  metaTitle[key] = row[1]
+  metaDescr[key] = row[2]
 
 # spit out the list
 print "var data = ["
 
+i=0
 for image in sorted(images):
-  img = str(image).strip()
-  
+  imgname = str(image).strip()
+  basename, ext = os.path.splitext(imgname)
+  img = Image.open(imgname)  
   # normal sized thumb
-  im = Image.open(img)
-  im.thumbnail((128, 128), Image.ANTIALIAS)
-  im.save("T_" + infile, "JPEG")
-  
-  thm = img.replace('.jpg', '_thumb.jpg')
-  sthm = img.replace('.jpg', '_smallthumb.jpg')
-  print "    {\n        image:'/art/resource/auroral_resources/chapman2011/"+thm+\
-  "',\n        link:'/art/resource/auroral_resources/chapman2011/"+img+\
-  "',\n        thumb:'/art/resource/auroral_resources/chapman2011/"+sthm+\
-  "',\n        title:'"+metaTitle[img]+\
-  "',\n        description:'"+metaDescr[img]+\
+  if not os.path.exists(basename + "_t" + ext):
+    img.thumbnail((800, 600), Image.ANTIALIAS)
+    img.save(basename + "_t" + ext, "JPEG")
+  # small sized thumb
+  if not os.path.exists(basename + "_st" + ext):
+    img.thumbnail((100, 75), Image.ANTIALIAS)
+    img.save(basename + "_st" + ext, "JPEG")
+  # prep the names for use
+  thm = basename + '_t' + ext
+  sthm = basename + '_st' + ext  
+  print "    {\n        image:'/art/resource/auroral_resources/chapman2011/" + thm +\
+  "',\n        link:'/art/resource/auroral_resources/chapman2011/" + imgname +\
+  "',\n        thumb:'/art/resource/auroral_resources/chapman2011/" + sthm +\
+  "',\n        title:'" + metaTitle[imgname] +\
+  "',\n        description:'" + metaDescr[imgname] +\
   "'\n    },\n"
+  if re.match(initialImage,imgname):
+    iInitial = i
+  i+=1
 
 print "];\n"
+print "var initialIndex = " + str(iInitial) + ";"
