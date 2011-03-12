c1a6d8b2ff0fb52f91c34914a3f02fd6259d37e7
peterelespuru <peterelespuru@4889a53b-78bf-f7fc-ca64-26766ea9d849>
2011-03-12 05:52:18 +0000

    added qx svn trunk

---
+++ trunk/qooxdoo-svn-trunk/framework/source/class/qx/bom/client/Browser.js
@@ -0,0 +1,252 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Christian Hagendorn (chris_schmidt)
+     * Martin Wittemann (martinwittemann)
+
+   ======================================================================
+
+   This class contains code from:
+
+     Copyright:
+       2009 Deutsche Telekom AG, Germany, http://telekom.com
+
+     License:
+       LGPL: http://www.gnu.org/licenses/lgpl.html
+       EPL: http://www.eclipse.org/org/documents/epl-v10.php
+
+     Authors:
+       * Sebastian Werner (wpbasti)
+
+************************************************************************ */
+
+/**
+ * Basic browser detection for qooxdoo. Based on "qx.client" variant for
+ * optimal performance and less overhead.
+ *
+ * This class is used by {@link qx.core.Environment} and should not be used 
+ * directly. Please check its class comment for details how to use it.
+ * 
+ * @internal
+ */
+qx.Bootstrap.define("qx.bom.client.Browser",
+{
+  statics :
+  {
+    /** 
+     * {Boolean} Whether the browser could not be determined 
+     * @deprecated since 1.4: See qx.core.Environment
+     */
+    UNKNOWN : true,
+
+    /** 
+     * {String} Name of the browser 
+     * @deprecated since 1.4: See qx.core.Environment
+     */
+    NAME : "unknown",
+
+    /** 
+     * {String} Combination of name and version e.g. "firefox 3.5" 
+     * @deprecated since 1.4: See qx.core.Environment
+     */
+    TITLE : "unknown 0.0",
+
+    /** 
+     * {Number} Floating point number of browser version 
+     * @deprecated since 1.4: See qx.core.Environment
+     */
+    VERSION : 0.0,
+
+    /** 
+     * {String} Full version. Might contain two dots e.g. "3.5.1" 
+     * @deprecated since 1.4: See qx.core.Environment
+     */
+    FULLVERSION : "0.0.0",
+
+
+    /**
+     * Checks for the name of the browser and returns it.
+     * @return {String} The name of the current browser.
+     * @internal
+     */
+    getName : function() {
+      var agent = navigator.userAgent;
+      var reg = new RegExp("(" + qx.bom.client.Browser.__agents + ")(/| )([0-9]+\.[0-9])");
+      var match = agent.match(reg);
+      if (!match) {
+        return "";
+      }
+
+      var name = match[1].toLowerCase();
+
+      var engine = qx.bom.client.Engine.getName();
+      if (engine === "webkit") {
+        // Fix Chrome name (which is still wrong defined in user agent on Android 1.6)
+        if (name === "android") {
+          name = "mobile chrome";
+        }
+
+        // Fix Safari name
+        else if (agent.indexOf("Mobile Safari") !== -1 || agent.indexOf("Mobile/") !== -1) {
+          name = "mobile safari";
+        }
+      }
+      else if (engine ===  "mshtml")
+      {
+        if (name === "msie")
+        {
+          name = "ie";
+
+          // Fix IE mobile before Microsoft added IEMobile string
+          if (qx.bom.client.OperatingSystem.getVersion() === "ce") {
+            name = "iemobile";
+          }
+        }
+      }
+      else if (engine === "opera")
+      {
+        if (name === "opera mobi") {
+          name = "operamobile";
+        } else if (name === "opera mini") {
+          name = "operamini";
+        }
+      }
+      
+      return name;   
+    },
+
+
+    /**
+     * Checks the version of the curren browser.
+     * @return {String} The name of the current browser.
+     * @internal
+     */
+    getVersion : function() {
+      var agent = navigator.userAgent;
+      var reg = new RegExp("(" + qx.bom.client.Browser.__agents + ")(/| )([0-9]+\.[0-9])");
+      var match = agent.match(reg);
+      if (!match) {
+        return "";
+      }
+      
+      var name = match[1].toLowerCase();
+      var version = match[3];
+
+      // Support new style version string used by Opera and Safari
+      if (agent.match(/Version(\/| )([0-9]+\.[0-9])/)) {
+        version = RegExp.$2;
+      }
+
+      if (qx.bom.client.Engine.getName() == "mshtml")
+      {
+        // Use the Engine version, because the IE8 and higher change the user agent
+        // string to a older version in compatibility mode
+        version = qx.bom.client.Engine.getVersion();
+
+        if (name === "msie" && qx.bom.client.OperatingSystem.getVersion() == "ce") {
+          // Fix IE mobile before Microsoft added IEMobile string
+          version = "5.0";
+        }
+      }
+
+      return version;
+    },
+    
+
+    /**
+     * Returns in which document mode the current document is (only for IE).
+     * 
+     * @internal
+     * @return {Number} The mode in which the browser is.
+     */
+    getDocumentMode : function() {      
+      if (document.documentMode) {
+        return document.documentMode;
+      }
+      return 0;
+    },
+
+
+    /**
+     * Check for quirks mode.
+     * 
+     * @internal
+     * @return {Boolean} <code>true</code>, if the environemt is in quirks mode
+     */
+    getQuirksMode : function() {
+      if(qx.bom.client.Engine.getName() == "mshtml" && 
+        parseFloat(qx.bom.client.Engine.getVersion()) >= 8) 
+      {
+        return qx.bom.client.Engine.DOCUMENT_MODE === 5;
+      } else {
+        return document.compatMode !== "CSS1Compat";
+      }
+    },
+
+
+    /**
+     * Internal herlper map for picking the right browser names to check.
+     */
+    __agents : {
+      // Safari should be the last one to check, because some other Webkit-based browsers
+      // use this identifier together with their own one.
+      // "Version" is used in Safari 4 to define the Safari version. After "Safari" they place the
+      // Webkit version instead. Silly.
+      // Palm Pre uses both Safari (contains Webkit version) and "Version" contains the "Pre" version. But
+      // as "Version" is not Safari here, we better detect this as the Pre-Browser version. So place
+      // "Pre" in front of both "Version" and "Safari".
+      "webkit" : "AdobeAIR|Titanium|Fluid|Chrome|Android|Epiphany|Konqueror|iCab|OmniWeb|Maxthon|Pre|Mobile Safari|Safari",
+
+      // Better security by keeping Firefox the last one to match
+      "gecko" : "prism|Fennec|Camino|Kmeleon|Galeon|Netscape|SeaMonkey|Firefox",
+
+      // No idea what other browsers based on IE's engine
+      "mshtml" : "IEMobile|Maxthon|MSIE",
+
+      // Keep "Opera" the last one to correctly prefer/match the mobile clients
+      "opera" : "Opera Mini|Opera Mobi|Opera"
+    }[qx.bom.client.Engine.getName()]
+  },
+
+
+  defer : function(statics) {
+    // @deprecated since 1.4: all code in the defer
+    statics.NAME = statics.getName();
+    statics.FULLVERSION = statics.getVersion();
+    statics.VERSION = parseFloat(statics.FULLVERSION, 10);
+    statics.TITLE = statics.NAME + " " + statics.VERSION;
+
+    if (statics.NAME !== "") {
+      statics.UNKNOWN = false;      
+    }
+    
+    // add @deprecation warnings    
+    var keys = ["FULLVERSION","VERSION","NAME","TITLE", "UNKNOWN"];
+    for (var i = 0; i < keys.length; i++) {
+      // check if __defineGetter__ is available
+      if (statics.__defineGetter__) {
+        var constantValue = statics[keys[i]];
+        statics.__defineGetter__(keys[i], qx.Bootstrap.bind(function(key, c) {
+          qx.Bootstrap.warn(
+            "The constant '"+ key + "' of '" + statics.classname + "'is deprecated: " +
+            "Plese check the API documentation of qx.core.Environemt.\n" + 
+            "Trace:" + qx.dev.StackTrace.getStackTrace().join("\n")
+          );
+          return c;
+        }, statics, keys[i], constantValue));
+      }
+    }
+  }
+});
