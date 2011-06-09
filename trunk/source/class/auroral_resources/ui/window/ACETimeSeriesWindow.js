9032341327c003971418e376c65d2219e7a520fa
peterelespuru <peterelespuru@4889a53b-78bf-f7fc-ca64-26766ea9d849>
2011-06-09 20:36:44 +0000

    adds ACE and CDAWeb dygraphs widgets with full loading and caching support

--- trunk/source/class/auroral_resources/ui/window/ACETimeSeriesWindow.js
+++ trunk/source/class/auroral_resources/ui/window/ACETimeSeriesWindow.js
@@ -37,7 +37,7 @@ or
 EPL: http://www.eclipse.org/org/documents/epl-v10.php
 
 AUTHOR(S) OF THIS FILE:
-Peter Elespuru - peter.elespuru@noaa.gov
+Peter R. Elespuru - peter.elespuru@noaa.gov
 
 *************************************************************************/
 
@@ -45,17 +45,26 @@ Peter Elespuru - peter.elespuru@noaa.gov
 qx.Class.define("auroral_resources.ui.window.ACETimeSeriesWindow",
 {
 
-    extend : qx.ui.window.Window,
+    /* 
+     * just a wrapper class for the specific plotter in use at this time to simplify
+     * switching between several different ones easily and quickly
+     */
+
+    extend : auroral_resources.ui.plot.dygraphs.TimeSeriesWindow,
 
     /*
     *****************************************************************************
-        STATICS
+        STATICS ARE NOT INHERITED!
     *****************************************************************************
     */
     statics : 
     {
+        getCsvUrl : function(parameter, start, stop) {
+            return "http://"+auroral_resources.Application.getHost()+"/spidr/servlet/GetData2?compress=true&format=csv_noheader&location=ALL&datefrom="+start+"&dateto="+stop+"&dataset="+parameter;
+        },
+
         fromArray : function(argArray) { 
-            return new auroral_resources.ui.window.ACETimeSeriesWindow(
+            return new auroral_resources.ui.plot.dygraphs.ACETimeSeriesWindow(
                 parseInt(decodeURI(argArray[3])), 
                 parseInt(decodeURI(argArray[4])), 
                 decodeURI(argArray[5]), 
@@ -63,280 +72,6 @@ qx.Class.define("auroral_resources.ui.window.ACETimeSeriesWindow",
                 decodeURI(argArray[7])
             );
         }
-    },
-
-
-    /*
-    *****************************************************************************
-        CONSTRUCTOR
-    *****************************************************************************
-    */
-    construct : function(width, height, parameter, title, mddocname)
-    {
-        this.base(arguments, title);
-
-        this.__timeBus = auroral_resources.messaging.TimeBus.getInstance();
-        this.__parameter = parameter;
-        this.__mddocname = mddocname;
-        this.__title = title;
-
-        this.set({
-            allowMaximize: false,
-            allowMinimize: false,
-            showMaximize: false,
-            showMinimize: false,
-            showClose: true,
-            status: parameter + ',' + title + ',' + mddocname,
-            layout: new qx.ui.layout.Grow()
-        });
-
-        this.setCaption(' (ACE Science Center) ' + this.getCaption());
-        
-        this.setWidth(width);
-        this.setHeight(height);
-
-        var start = this.__timeBus.getStartDateForSPIDRWS2();
-        var stop = this.__timeBus.getStopDateForSPIDRWS2();
-
-        this.__startDate = start;
-        this.__stopDate = stop;
-
-        this.__plot = new qxdygraphs.Plot(
-            "http://spidr.ngdc.noaa.gov/spidr/servlet/GetData2?compress=true&format=csv_noheader&location=ALL&datefrom="+start+"&dateto="+stop+"&dataset="+parameter,
-            {
-                labelsKMB: true,
-                drawPoints: true,
-                errorBars: false,
-                lables: [title],
-                highlightCircleSize: 3,
-                strokeWidth: 1,
-                underlayCallback: this._vline,
-                zoomCallback: this._zoom
-            }
-        );
-
-        this.add(this.__plot);
-        
-        this.addListener("close", function(evt) { this.destroy() });
-        this.addListener("mouseup", this._rightClick, this);
-        this.__timeBus.getBus().subscribe("time.startDate", this._startDateChangeBusCallback, this);
-        this.__timeBus.getBus().subscribe("time.now", this._nowChangeBusCallback, this);
-        this.__timeBus.getBus().subscribe("time.stopDate", this._stopDateChangeBusCallback, this);
-
-        return this;
-    },
-
-
-    /*
-    *****************************************************************************
-        CLASS VARIABLES AND METHODS
-    *****************************************************************************
-    */
-    members :
-    {
-        __title : null,
-        __parameter : null,
-        __mddocname : null,
-        __timeBus : null,
-        __startDate : null,
-        __stopDate : null,
-        __plot : null,
-        __now : null,
-        __csvData : null,
-        
-
-        //
-        //
-        //
-        _rightClick : function(evt) { 
-            if(evt.isRightPressed()) {
-                
-                var popup = new qx.ui.popup.Popup(new qx.ui.layout.VBox()).set({
-                     autoHide: true
-                });
-                
-                var param = this.__parameter;
-                var start = this.__startDate;
-                var stop = this.__stopDate;
-                var mddoc = this.__mddocname;
-                
-                var data = new qx.ui.form.Button("Download Data");
-                data.addListener("click", function(evt) {
-                    alert('Not Yet Available');
-                    /*
-                    var dlurl = "http://spidr.ngdc.noaa.gov/spidr/servlet/GetData?param="+param+"&format=zip&dateFrom="+start+"&dateTo="+stop;
-                    window.open(dlurl,"");
-                    popup.hide();
-                    */
-                });
-                
-                var mdata = new qx.ui.form.Button("View Metadata");
-                mdata.addListener("click", function(evt) {
-                    alert('Not Yet Available');
-                    /*                    
-                    var mdurl = "http://spidr.ngdc.noaa.gov/spidrvo/viewdata.do?docname="+mddoc;
-                    window.open(mdurl,"");
-                    popup.hide();
-                    */
-                });
-                
-                popup.add(new qx.ui.basic.Label("Additional Options"));
-                popup.add(data);
-                popup.add(mdata);
-                popup.placeToMouse(evt);
-                popup.show();            
-            }
-        },
-        
-        
-        //
-        //
-        //
-        _zoom : function(minDate, maxDate, minValue, maxValue) {
-            // not doing anything here yet
-        },
-        
-        
-        //
-        // vertical line function
-        //
-        _vline : function(canvas, area, g) {
-            /* no need to normalize, just use toDomCoords !!!
-            // alert(area.toSource()); // MUST BE RUN IN FIREFOX! => debug output
-            // note the way this method is registered as a callback prevents access
-            // to class variables, must access them anew (e.g. timeBus)
-            var timeBus = auroral_resources.messaging.TimeBus.getInstance();
-            var start = timeBus.getStartDate();
-            var stop = timeBus.getStopDate();
-            var now = timeBus.getNow();
-            
-            var A = start;
-            var B = stop;
-            var C = area.x+1;
-            var D = area.w+area.x;
-            // normalize x-prime to determine how the time maps to pixel space
-            var xp = ((D-C)*(now-A)) / (B-A) + C;
-            */
-            
-            var timeBus = auroral_resources.messaging.TimeBus.getInstance();
-            var now = timeBus.getNow();
-//            now = now + ((new Date().getTimezoneOffset()*60)*1000);
-            var xp = g.toDomCoords(parseInt(now),0); //only care about X
-            xp = xp[0];
-            
-            canvas.beginPath();
-            canvas.strokeStyle = "rgba(255, 0, 0, 1.0)";
-            canvas.moveTo(xp, 0);
-            canvas.lineTo(xp, area.h);
-            canvas.closePath();
-            canvas.stroke();
-        },
-
-        //
-        // callback for the 'startDate' message channel
-        //
-        _startDateChangeBusCallback : function(e) {
-
-            this.remove(this.__plot);
-
-            this.__plot.getPlotObject().destroy();
-            qx.util.DisposeUtil.disposeObjects(this, "__plot", false);
-            this.__plot = null;
-            
-            var start = this.__timeBus.convertToSPIDRWS2(e.getData());
-            this.__start = start;
-            var stop = this.__timeBus.getStopDateForSPIDRWS2();
-            var now = this.__timeBus.getNow();
-            var parameter = this.__parameter;
-            
-            this.__plot = new qxdygraphs.Plot(
-                "http://spidr.ngdc.noaa.gov/spidr/servlet/GetData2?compress=true&format=csv_noheader&location=ALL&datefrom="+start+"&dateto="+stop+"&dataset="+parameter,
-                {
-                    labelsKMB: true,
-                    errorBars: false,
-                    drawPoints: true,
-                    lables: [this.__title],
-                    highlightCircleSize: 3,
-                    strokeWidth: 1,
-                    underlayCallback: this._vline,
-                    zoomCallback: this._zoom
-                }
-            );
-
-            this.add(this.__plot);      	    
-        },
-
-        //
-        // callback for the 'stopDate' message channel
-        //
-        _stopDateChangeBusCallback : function(e) {
-
-            this.remove(this.__plot);
-
-            this.__plot.getPlotObject().destroy();
-            qx.util.DisposeUtil.disposeObjects(this, "__plot", false);
-            this.__plot = null;
-            
-            var start = this.__timeBus.getStartDateForSPIDRWS2();
-            var stop = this.__timeBus.convertToSPIDRWS2(e.getData());
-            this.__stop = stop;
-            var now = this.__timeBus.getNow();
-            var parameter = this.__parameter;
-
-            this.__plot = new qxdygraphs.Plot(
-                "http://spidr.ngdc.noaa.gov/spidr/servlet/GetData2?compress=true&format=csv_noheader&location=ALL&datefrom="+start+"&dateto="+stop+"&dataset="+parameter,
-                //'resource/auroral_resources/ionofof2.txt',
-                {
-                    labelsKMB: true,
-                    errorBars: false,
-                    drawPoints: true,
-                    lables: [this.__title],
-                    highlightCircleSize: 3,
-                    strokeWidth: 1,
-                    underlayCallback: this._vline,
-                    zoomCallback: this._zoom
-                }
-            );
-
-            this.add(this.__plot);
-        },	
-
-        //
-        //
-        //
-        _nowChangeBusCallback : function(e) {
-
-            var g = this.__plot.getPlotObject();
-            var start = this.__timeBus.getStartDateForSPIDRWS2();
-            var stop = this.__timeBus.getStopDateForSPIDRWS2();
-            var now = e.getData();
-            this.__now = now;
-            var parameter = this.__parameter;
-            var csv = "http://spidr.ngdc.noaa.gov/spidr/servlet/GetData2?compress=true&format=csv_noheader&location=ALL&datefrom="+start+"&dateto="+stop+"&dataset="+parameter;
- 
-            var h = new qx.io.request.Xhr();
-            h.setAsync(true);
-
-            h.addListener("success", function() {
-                g.updateOptions({ 'file' : h.responseText });
-            });
-
-            h.setMethod("GET");
-            h.setUrl(csv);
-            h.send();
-        }
-    },
-
-
-    /*
-    *****************************************************************************
-        DESTRUCTOR
-    *****************************************************************************
-    */
-    destruct : function()
-    {
-        // TODO: add destructor code...
     }
-
 
 });
